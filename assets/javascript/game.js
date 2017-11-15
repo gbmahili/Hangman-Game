9574800096b3ca704334973cf5693aa72e3c31f8
gbmahili <georgesmahili@gmail.com>
2017-11-15 01:13:04 -0500

    testing the game.js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,20 @@
-var words = ["madonamama", "jackson"];
+var words = ["Baraka", "Mahili"];
+
+var remainingGuessCount = 6;
+
+var remainingGuessCountDiv = document.getElementsByClassName("remaining-guess-count");
 
 document.getElementById("current-word").innerHTML = words[1];
+
+document.onkeyup = function (event) {
+    remainingGuessCount = remainingGuessCount - 1;
+    console.log(remainingGuessCountDiv);
+}
+
+//User presses a key, check if that key is in the first word of the array
+//We decrease the remaining number of guesses by one
+
+    //If it is there, 
+            //we get the index of that letter in from the word
+
+            //Then, we replace the CORRESPONDING index of the current-word id
